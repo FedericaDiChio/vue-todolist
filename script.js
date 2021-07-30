1a8fcbedbef4973b02acd81ee9393e3d03d49c68
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-30 15:44:04 +0200

    lista, btn, input

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 
 const root = new Vue ({
     el: "#root",
-    data: {},
-    methods: {},
+    data: {
+        newItem: "",
+        toDo: [
+            "Fare la spesa",
+            "Pulire la libreria",
+            "Portare il cane a fare una passeggiata",
+            "Studiare",
+        ],
+    },
+    methods: {
+        deleteItem(index){
+            this.toDo.splice(index, 1);
+        },
+        addItem(){
+            this.toDo.push(this.newItem);
+            this.newItem = " ";
+        },
+    },
 });
